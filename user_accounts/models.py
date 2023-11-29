50f4b6e31287758a842a8363227c3956eae4bd91
tobest242 <janetabioye242@gmail.com>
2023-11-29 11:11:31 +0100

    user created

---
+++ user_accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+
+class User(AbstractBaseUser, PendingDeprecationWarning):
+    email=models.EmailField(max_length=255, unique=True, verbose_name=("Email Address"))
+    first_name = models.CharField(max_length=100, verbose_name=("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name=("Last Name"))
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS: ("first_name", "last_name")
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+    def tokens(self):
+        pass
